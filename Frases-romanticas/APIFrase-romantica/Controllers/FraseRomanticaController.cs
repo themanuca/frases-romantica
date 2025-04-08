a99a7a1571e28a5bd2a2674712fb3d6973a0de8e
Sergio Manuca <ssergioca@gmail.com>
2025-04-08 08:03:48 -0300

    Controller iniciais criada, ainda com uso do model, mas será substuidos por dtos

---
+++ Frases-romanticas/APIFrase-romantica/Controllers/FraseRomanticaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using App.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Domain.Entities.Models;
+namespace APIFrase_romantica.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FraseRomanticaController : ControllerBase
+    {
+        private readonly IFraseRomanticaServices _fraseRomanticaServices;
+
+        public FraseRomanticaController(IFraseRomanticaServices fraseRomanticaServices)
+        {
+            _fraseRomanticaServices = fraseRomanticaServices;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListarFrases()
+        {
+            var lista = await _fraseRomanticaServices.ObterTodasAsync();
+            if (!lista.Any())
+            {
+                return NotFound("Frases não encontrada.");
+            }
+            return Ok(lista);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult>ObterFrase(int id)
+        {
+             var frase = await _fraseRomanticaServices.ObterPorIdAsync(id);
+            if(frase?.Texto == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(frase);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> SalvarFrase(FraseRomantica frase)
+        {
+            await _fraseRomanticaServices.CriarAsync(frase);
+            return Ok();
+        }
+    }
+}
